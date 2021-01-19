f978701ace46233d88a2e639d97d383523ef07c6
Matthew Starcher <mlstarcher@gmail.com>
2021-01-19 13:12:14 -0800

    Continuing refactor, have created pre header and placed Jordan and Converse logos

---
+++ client/src/components/Header/PreLHeader/PreLHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import LogoContainer from './LogoContainer.jsx'
+
+const PreLHeader = () => (
+  <div className="pre-l-brand-header d-sm-h d-lg-b z3">
+    <div className="pre-l-wrapper maut-sm d-sm-flx flx-jc-sm-sb flx-wr-sm-nw flx-dir-sm-r">
+    <LogoContainer />
+      {/* Logo container
+      Nav Bar */}
+    </div>
+    {/* <LogoContainer />
+    <NavSelectors /> */}
+  </div>
+);
+
+export default PreLHeader;
