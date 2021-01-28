2fd625d2eff7179f5d2c3c4c2bd26f52c1b0e851
Matthew Starcher <mlstarcher@gmail.com>
2021-01-28 12:07:03 -0800

    Prepped repo for containerization

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,11 @@
 const Axios = require('axios');
 const Inventory = require('./index.js');
+const mongooge = require('mongoose');
 
 const seed = () => {
   Axios.get('https://api.thesneakerdatabase.com/v1/sneakers?limit=100&name=Jordan&brand=nike')
     .then((res) => Inventory.create(res.data.results))
+    .then(() => mongoose.disconnect())
     .catch((err) => console.error(err));
 };
 
